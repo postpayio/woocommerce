425b785895d20fef49c46cb5ebc093892cca7d61
mongkok <domake.io@gmail.com>
2020-03-30 15:27:07 +0700

    Modified cart widget type

--- templates/widgets/cart.php
+++ templates/widgets/cart.php
@@ -10,7 +10,7 @@ $amount = WC_Postpay_Adapter::decimal( WC()->cart->total );
 
 <div
 	class="postpay-widget"
-	data-type="product"
+	data-type="cart"
 	data-amount="<?php echo $amount->jsonSerialize(); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>"
 	data-currency="<?php echo get_woocommerce_currency(); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>"
 ></div>
