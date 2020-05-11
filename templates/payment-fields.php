e2f9ea8b40ec3ac6d80a34c33a2637f04f0fada3
mongkok <domake.io@gmail.com>
2020-05-12 04:25:47 +0700

    Ignored OutputNotEscaped rule

--- templates/payment-fields.php
+++ templates/payment-fields.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
 	data-type="instalment-plan"
 	data-amount="<?php echo WC_Postpay_Adapter::decimal( WC()->cart->total )->jsonSerialize(); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>"
 	data-currency="<?php echo get_woocommerce_currency(); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>"
-	data-country="<?php echo WC()->customer->get_shipping_country(); ?>"
+	data-country="<?php echo WC()->customer->get_shipping_country(); // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>"
 	data-hide-if-invalid="<?php echo $gateway->css; // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped ?>"
 ></div>
 
