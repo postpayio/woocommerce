76edec097c94a95d6762a8d55ba7091d1951a78a
Nabhan18 <106145125+Nabhan18@users.noreply.github.com>
2023-02-13 15:55:03 +0400

    Tested up to bumped to 6.0 in tagged good release 0.2.3

--- postpay.php
+++ postpay.php
@@ -10,7 +10,7 @@
  * Developer URI: https://github.com/mongkok
  *
  * Requires at least: 4.4
- * Tested up to: 5.4
+ * Tested up to: 6.0
  * WC requires at least: 3.0
  * WC tested up to: 4.1.1
  *
